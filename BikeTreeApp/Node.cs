e289cc791ce4032b8076883d86ff2cbc6d840df8
Mohammad Anouti <manouti1988@gmail.com>
2023-12-25 15:58:51 +0200

    max bundle calculation implmentation

---
+++ BikeTreeApp/Node.cs
@@ -0,0 +1,37 @@
+﻿namespace BikeTreeApp
+{
+    public class Node
+    {
+        //part is pedal,wheel,seat
+        public string PartName { get; }
+        //available inventory of this part
+        public int Inventory { get; set; }
+        public Node[] Children { get; set; }
+
+        public Node(string partName, int inventory)
+        {
+            PartName = partName;
+            Inventory = inventory;
+        }
+
+        public int CalculateMaxBundles(int bundlesPerNode)
+        {
+            // If the part is a leaf node, return the available inventory divided by the bundles per node
+            if (Children == null || Children.Length == 0)
+            {
+                return Inventory / bundlesPerNode;
+            }
+
+            // If the part has children, calculate the maximum bundles based on each child
+            int maxBundles = int.MaxValue;
+            foreach (var child in Children)
+            {
+                int bundlesFromChild = child.CalculateMaxBundles(bundlesPerNode);
+                maxBundles = Math.Min(maxBundles, bundlesFromChild);
+            }
+
+            return maxBundles;
+        }
+    }
+
+}
---
+++ BikeTreeApp/Program.cs
@@ -0,0 +1,19 @@
+﻿// Build a tree structure representing the bike assembly
+using BikeTreeApp;
+
+Node bike = new Node("Bike", int.MaxValue);
+Node seat = new Node("Seat", 50);
+Node pedals = new Node("Pedals", 60);
+Node wheels = new Node("Wheels", int.MaxValue);
+Node frame = new Node("Frame", 60);
+Node tube = new Node("Tube", 35);
+
+wheels.Children = new Node[] { frame, tube };
+bike.Children = new Node[] { seat, pedals, wheels };
+
+// Calculate the maximum number of bike bundles that can be built
+int maxBundles = bike.CalculateMaxBundles(1);
+
+// Display the result to the user
+Console.WriteLine($"You can build a maximum of {maxBundles} complete bike bundles with the available parts.");
+
